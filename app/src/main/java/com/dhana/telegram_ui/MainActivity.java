29379404464a3100bf4f10f9f6bf5d7583d2970e
devpdhana <devpdhanasekar@gmail.com>
2022-10-31 09:36:14 +0530

    Telegram_UI_Designs

--- app/src/main/java/com/dhana/telegram_ui/MainActivity.java
+++ app/src/main/java/com/dhana/telegram_ui/MainActivity.java
@@ -6,11 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.TablayoutMain);
+        tabLayout.addTab(tabLayout.newTab().setText("CHATS"));
+        tabLayout.addTab(tabLayout.newTab().setText("GROUPS"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 }
--- app/src/main/java/com/dhana/telegram_ui/Splash_Screen.java
+++ app/src/main/java/com/dhana/telegram_ui/Splash_Screen.java
@@ -1,8 +1,11 @@
 package com.dhana.telegram_ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Screen extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_Screen.this,Welcome_Activity.class));
+                finish();
+            }
+        },2000);
     }
 }
